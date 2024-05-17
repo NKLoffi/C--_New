1b05636ba4008e48a05e4523cc442a23fbc99a55
Nikhil <nklajiofficial@gmail.com>
2024-05-17 13:57:05 -0400

    pyramid code

---
+++ Examples/pyramid.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+ char syb = '*';
+    for(int i =0; i<=10; ++i){
+        for(int j =0; j<=i; ++j){
+            
+        std::cout<<syb;
+        }
+        std::cout<<'\n';
+        
+    }
+}
