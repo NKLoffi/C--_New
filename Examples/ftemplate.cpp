36a3b0dfc91ab4c1c21cee6d2f59bb87227b9edf
Nikhil <nklajiofficial@gmail.com>
2024-05-22 04:24:17 -0400

    added more code to function template

--- Examples/ftemplate.cpp
+++ Examples/ftemplate.cpp
@@ -1,9 +1,19 @@
+// template <typename T>
+// T max(T x, T y){
+//     return (x>y)?x:y;
+// }
+// int main(){
+//     std::cout<<max ('1', '6')<<'\n';
+//     return 0;
+// }
+
 #include<iostream>
-template <typename T>
-T max(T x, T y){
+template<typename T, typename U>
+auto max(T x, U y){
     return (x>y)?x:y;
+
 }
 int main(){
-    std::cout<<max ('1', '6')<<'\n';
+    std::cout<< max(1, 6.2)<<'\n';
     return 0;
 }
